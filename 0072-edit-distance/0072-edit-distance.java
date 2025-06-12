75af71f4bff1334d948a153f2bd71ec38d877e7b
Archisman SinhaRoy <archisman.sroy2003@gmail.com>
2025-06-13 02:02:29 +0530

    Time: 2 ms (100%), Space: 45.3 MB (30.41%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,25 +1,29 @@
-// Etao toka.. porashona koro
+class Solution 
+{
+    int f(int i, int j, String s1, String s2, int[][] dp)
+    {
+        if(i < 0) return j+1;
 
-class Solution {
-  public int minDistance(String word1, String word2) {
-    final int m = word1.length();//first word length
-    final int n = word2.length();///second word length
-    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
-    int[][] dp = new int[m + 1][n + 1];
+        if(j < 0) return i+1;
 
-    for (int i = 1; i <= m; ++i)
-      dp[i][0] = i;
+        if(dp[i][j] != -1) return dp[i][j];
 
-    for (int j = 1; j <= n; ++j)
-      dp[0][j] = j;
+        if(s1.charAt(i) == s2.charAt(j))
+        return dp[i][j] = f(i-1, j-1, s1, s2, dp);
 
-    for (int i = 1; i <= m; ++i)
-      for (int j = 1; j <= n; ++j)
-        if (word1.charAt(i - 1) == word2.charAt(j - 1))//same characters
-          dp[i][j] = dp[i - 1][j - 1];//no operation
-        else
-          dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;                      //replace               //delete        //insert
+        return dp[i][j] = 1 + Math.min(f(i-1, j, s1, s2, dp), Math.min(f(i, j-1, s1, s2, dp), f(i-1, j-1, s1, s2, dp)));
+    }
+    public int minDistance(String word1, String word2) 
+    {
+        int n = word1.length();
+        int m = word2.length();
 
-    return dp[m][n];
-  }
+        int[][] dp = new int[n][m];
+        for (int[] row : dp) {
+            java.util.Arrays.fill(row, -1);
+        }
+
+        return f(n-1, m-1, word1, word2, dp);
+        
+    }
 }
