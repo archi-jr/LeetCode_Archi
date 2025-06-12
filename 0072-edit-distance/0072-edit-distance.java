993c9505916fa834e4c14b43d9458975c58e1164
Archisman SinhaRoy <archisman.sroy2003@gmail.com>
2025-06-13 02:22:04 +0530

    Time: 5 ms (61.68%), Space: 45.5 MB (11.12%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,29 +1,38 @@
+//Bottom-Up Approach
 class Solution 
 {
-    int f(int i, int j, String s1, String s2, int[][] dp)
-    {
-        if(i < 0) return j+1;
-
-        if(j < 0) return i+1;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s1.charAt(i) == s2.charAt(j))
-        return dp[i][j] = f(i-1, j-1, s1, s2, dp);
-
-        return dp[i][j] = 1 + Math.min(f(i-1, j, s1, s2, dp), Math.min(f(i, j-1, s1, s2, dp), f(i-1, j-1, s1, s2, dp)));
-    }
     public int minDistance(String word1, String word2) 
     {
         int n = word1.length();
         int m = word2.length();
 
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n+1][m+1];
         for (int[] row : dp) {
-            java.util.Arrays.fill(row, -1);
+            java.util.Arrays.fill(row, 0);
+        }
+        //Tabulation
+        for(int i=0; i<=n; i++)
+        {
+            dp[i][0] = i;
         }
 
-        return f(n-1, m-1, word1, word2, dp);
+        for(int j=0; j<=m; j++)
+        {
+            dp[0][j] = j;
+        }
+
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                if(word1.charAt(i-1) == word2.charAt(j-1))
+                 dp[i][j] = dp[i-1][j-1];
+
+                else dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]));
+            }
+        }
+
+        return dp[n][m];
         
     }
 }
