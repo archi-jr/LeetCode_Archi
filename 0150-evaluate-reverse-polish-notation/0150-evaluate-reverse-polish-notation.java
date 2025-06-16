eee385b148e4f57f2e3ea8df020041010bb1123d
Archisman SinhaRoy <archisman.sroy2003@gmail.com>
2025-06-17 01:38:08 +0530

    Time: 5 ms (97.29%), Space: 44.7 MB (53.61%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,27 +1,37 @@
-// Eta tukechi.. so eta abr dakho
-
 class Solution {
-    public int evalRPN(String[] tokens) {
+    public int evalRPN(String[] tokens) 
+    {
         Stack<Integer> stack = new Stack<>();
-
-        for (String c : tokens) {
-            if (c.equals("+")) {
-                stack.push(stack.pop() + stack.pop());
-            } else if (c.equals("-")) {
-                int second = stack.pop();
-                int first = stack.pop();
-                stack.push(first - second);
-            } else if (c.equals("*")) {
-                stack.push(stack.pop() * stack.pop());
-            } else if (c.equals("/")) {
-                int second = stack.pop();
-                int first = stack.pop();
-                stack.push(first / second);
-            } else {
-                stack.push(Integer.parseInt(c));
+        
+        for (String token : tokens) 
+        {
+            if (isOperator(token)) 
+            {
+                int b = stack.pop();//2nd element
+                int a = stack.pop();//1st element
+                stack.push(calculate(a, b, token));
+            } 
+            else 
+            {
+                stack.push(Integer.parseInt(token));
             }
         }
-
-        return stack.peek();        
+        
+        return stack.peek();
+    }
+    
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || 
+               token.equals("*") || token.equals("/");
+    }
+    
+    private int calculate(int a, int b, String operator) {
+        switch (operator) {
+            case "+": return a + b;
+            case "-": return a - b;
+            case "*": return a * b;
+            case "/": return a / b;
+            default: throw new IllegalArgumentException("Invalid operator");
+        }
     }
 }
